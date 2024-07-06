bd7ccef20aeb2adfcc79f978e916725188104772
Pankaj Mishra <112557161+PankajMishra99@users.noreply.github.com>
2024-07-06 08:26:28 +0000

    Converiosn of Data Types

---
+++ basic_test/conversion_opertion.js
@@ -0,0 +1,13 @@
+/*let score = true
+// console.log(typeof score)
+
+let value = "44abc"
+value_in_number=Number(score)
+console.log(value_in_number)
+*/
+let isloggedin= ""
+let booleanloggedin = Boolean(isloggedin)
+console.log(booleanloggedin)
+
+// 1=> True, 0 => False
+//  "Anything Name"=> True,""=>False
