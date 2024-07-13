758ded6af12f87663acbfe2a061afe6f50547914
Pankaj Mishra <112557161+PankajMishra99@users.noreply.github.com>
2024-07-13 04:02:27 +0000

    function

---
+++ basic_test/function.js
@@ -0,0 +1,38 @@
+// function x (a1,a2) {
+// console.log(a1+a2)
+// return (a1+a2)
+
+// }
+// const result = x(2,4)
+// console.log("Result :", result)
+
+
+// function loggedin (username ="Pankaj") {
+//     if (!username){
+//          console.log("Please enter a username")
+//          return 
+//     }
+// return `${username} just logged in`
+
+// }
+// console.log(loggedin(""))
+
+// function calculatecart(val1,val2,...num1){
+//     return (val1,val2,num1)
+
+// }
+// // console.log(calculatecart(200,400,500,2000))
+// const user= {
+//     username:"Pankaj",
+//     prices : 199
+// }
+// function handleobject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+// console.log(handleobject(user))
+
+const getarray = [1,2,4,5,6,7]
+function myarray(arr1){
+    return arr1[4]
+}
+console.log(myarray(getarray))
